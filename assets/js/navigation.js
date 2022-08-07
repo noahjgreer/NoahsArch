790623e6487c671041dd69d8b5832e5480c45d42
Noah Greer <noahjgreer@gmail.com>
2022-08-07 15:22:58 -0700

    v2.0.0

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,6 +1,60 @@
-const burger = document.querySelector('.hamburger');
-const navText = document.querySelector('.navtext');
-const navSide = document.querySelector('.side');
+var burger = document.querySelector('.hamburger');
+var navText = document.querySelector('.navtext');
+var navSide = document.querySelector('.side');
+var navFooter = document.querySelector('.navigation');
+var copyright = document.querySelector('footer .bottom');
+// var headerAll = document.getElementById('navtop'); 
+var pageData;
+
+
+// Page Retrieval
+async function updatePages() {
+    var pageDataResponse = await fetch('/assets/json/pages.json');
+    var pageData = await pageDataResponse.json();
+
+    var navText_Updated = "";
+    var pageCount = pageData["pages"].length;
+
+    for (let i = 0; i < pageCount; i++) {
+        var navText_Updated = navText_Updated + `
+        <a href="${pageData["pages"][i]["dst"]}">${pageData["pages"][i]["name"]}</a>`
+
+        console.log(navText_Updated);
+        // console.log(pageData["pages"][i]);
+    }
+
+    // headerAll.innerHTML = `
+    // <div class="hamburger">
+    //     <div class="top"></div>
+    //     <div class="mid"></div>
+    //     <div class="end"></div>
+    // </div>
+    // <div class="side">
+    //     ${navText_Updated}
+    // </div>
+    // <div class="navtext">
+    //     ${navText_Updated}
+    // </div>
+    // `
+
+    // console.log(headerAll);
+
+    navText.innerHTML = navText_Updated;
+    navSide.innerHTML = navText_Updated;
+    navFooter.innerHTML = navText_Updated;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Retrieve Pages
+    updatePages();
+    updateCopyright();
+
+    // Burger Setup
+    if (window.getComputedStyle(navText).visibility == "hidden") {
+        burger.classList.add('visible');
+    }
+})
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('toggle');
@@ -18,11 +72,12 @@ function isInViewport(element) {
     );
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.getComputedStyle(navText).visibility == "hidden") {
-        burger.classList.add('visible');
-    }
-})
+function updateCopyright() {
+    copyright.innerHTML = `
+    <p>Copyright 2021-${new Date().getFullYear()} © Noah Greer</p>
+    `
+}
+
 
 document.addEventListener('scroll', () => {    
     if (navText.getBoundingClientRect().top <= -50) {
